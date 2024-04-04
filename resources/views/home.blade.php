e26de70847380520720a8145f1b3e288d15fbc83
Shamil <shamilyassin799@gmail.com>
2024-04-04 15:50:32 +0700

    Menambahkan components dan gambar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,21 @@
-<div>
-    Selamat datang
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>@yield('title', 'My App')</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" />
+</head>
+<body>
+    <header>
+        @include('components.header')
+    </header>
+    <div class="container">
+        <main>
+            @yield('content')
+        </main>
+    </div>
+    <footer>
+    @include('components.carousel')
+    </footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+</body>
+</html>
