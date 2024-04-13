d528ead514671a10a70df28955356ba9aefba39c
Shamil <shamilyassin799@gmail.com>
2024-04-13 22:24:15 +0700

    Menambahkan

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
-    {
-        return view('home');
+
+    public function dashboard(){
+        return view('dashboard');
+    }
+
+    public function index(){
+
+        $data = User::get();
+
+        return view('index',compact('data'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\konsumenController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'dashboard']);
 Route::get('/about', [AboutController::class, 'about'])->name('about');
 Route::get('/aplikasi', [AplikasiController::class, 'aplikasi'])->name('aplikasi');
 Route::get('/contact', [ContactController::class, 'contact'])->name('contact');
-Route::resource('konsumen',konsumenController::class);
